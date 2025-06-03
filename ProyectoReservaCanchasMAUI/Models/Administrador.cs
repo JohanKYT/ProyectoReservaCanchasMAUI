74ae058437b7a86034ec7233f6b9ac26e3194a30
JohanKYT <kevin.maquis@udla.edu.ec>
2025-06-03 17:20:57 -0500

    Se agregaron los primeros modelos PersonaUdla, Administrador, Facultad, Carrera para realizar la primera conexion con las APIs

--- ProyectoReservaCanchasMAUI/Models/Administrador.cs
+++ ProyectoReservaCanchasMAUI/Models/Administrador.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ProyectoReservaCanchasMAUI.Models.NewFolder
+namespace ProyectoReservaCanchasMAUI.Models
 {
     public class Administrador : PersonaUdla
     {
