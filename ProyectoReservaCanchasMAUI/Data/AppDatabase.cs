e192e4978f08aad25905c1e10db40dd62fbe81a5
JohanKYT <kevin.maquis@udla.edu.ec>
2025-07-11 15:18:53 -0500

    Implementación del modelo Campus y la clase de base de datos AppDatabase para SQLite Se modifica la clase Campus estableciendo la clave primaria Id con autoincremento utilizando SQLite.  Se establece una conexión asíncrona con SQLite mediante SQLiteAsyncConnection.  Se agrega un método ObtenerCampusAsync, GuardarCampusAsync, GuardarCampusAsync. Con esta implementación, la aplicación ahora soporta operaciones CRUD completas en la base de datos local con SQLite, incluyendo la capacidad de actualizar registros existentes.

---
+++ ProyectoReservaCanchasMAUI/Data/AppDatabase.cs
@@ -0,0 +1,36 @@
+﻿using ProyectoReservaCanchasMAUI.Models;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoReservaCanchasMAUI.Data
+{
+    public class AppDatabase
+    {
+        private readonly SQLiteAsyncConnection _database;
+
+        public AppDatabase(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Campus>().Wait();
+        }
+
+        public Task<List<Campus>> ObtenerCampusAsync()
+        {
+            return _database.Table<Campus>().ToListAsync();
+        }
+
+        public Task<int> GuardarCampusAsync(Campus campus)
+        {
+            return campus.Id == 0 ? _database.InsertAsync(campus) : _database.UpdateAsync(campus);
+        }
+
+        public Task<int> EliminarCampusAsync(Campus campus)
+        {
+            return _database.DeleteAsync(campus);
+        }
+    }
+}
--- ProyectoReservaCanchasMAUI/Models/Campus.cs
+++ ProyectoReservaCanchasMAUI/Models/Campus.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,11 @@ namespace ProyectoReservaCanchasMAUI.Models
 {
     public class Campus
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+        [MaxLength(100)]
         public string Nombre { get; set; }
+        [MaxLength(100)]
         public string Direccion { get; set; }
     }
 }
